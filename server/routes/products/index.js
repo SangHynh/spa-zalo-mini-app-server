088fbdf462b3cf7c0bc259e70483c37ff600b969
MPhong03 <dangminhphong912@gmail.com>
2024-09-17 14:48:43 +0700

    Init CMS

--- server/routes/products/index.js
+++ server/routes/products/index.js
@@ -4,8 +4,11 @@ const productController = require('../../controllers/product.controller');
 const upload = require('../../middlewares/upload.middlewares');
 const MAX_FILES = 10;
 
+// GET
 router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
 
+// POST 
 router.post('/', (req, res, next) => {
     req.folder = process.env.PRODUCT_FOLDER; // Folder name in cloud
     next();
@@ -14,8 +17,10 @@ router.post('/', (req, res, next) => {
     productController.createProduct
 );
 
-router.get('/:id', productController.getProductById);
+// PUT
 router.put('/:id', productController.updateProduct);
+
+// DELETE
 router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
