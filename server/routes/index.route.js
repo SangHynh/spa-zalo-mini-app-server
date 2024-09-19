d8c9b5ff2c39592720db39b3935dab48951e446a
SangHynh <huynhvansang.020102@gmail.com>
2024-09-19 23:07:06 +0700

    feat: Implement access token verification for API routes
    - Added access token verification to all API routes.

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -2,12 +2,14 @@ const student = require("./student.route")
 const test = require("./test.route")
 const product = require('./products')
 const user = require('./users')
+const { verifyAccessToken } = require("../configs/jwt.config");
+
 
 const initRoutes = (app) => {
-    app.use("/api/students", student)
-    app.use("/api/test", test)
-    app.use('/api/products',product)
-    app.use('/api/users',user)
+    app.use("/api/students",verifyAccessToken, student)
+    app.use("/api/test",verifyAccessToken, test)
+    app.use('/api/products',verifyAccessToken, product)
+    app.use('/api/users',verifyAccessToken, user)
 }
 
 module.exports = initRoutes
