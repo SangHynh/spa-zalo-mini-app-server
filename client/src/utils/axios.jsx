c929a2d1672ffb3ac41d00114f64aa2d55d5c835
MPhong03 <dangminhphong912@gmail.com>
2024-09-21 13:48:18 +0700

    build: merge CMS to main

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { getCookie } from "./cookies";
 
+// CUSTOM AXIOS WITH HEADER CONTAIN TOKEN
 const instance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 });
