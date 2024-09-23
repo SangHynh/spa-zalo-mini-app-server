484b88d68933d100822eb93e30b70778809a6ca3
MPhong03 <dangminhphong912@gmail.com>
2024-09-23 21:27:32 +0700

    feat(api): finish CRUD product, handle delete images in cloudinary

--- server/routes/products/index.js
+++ server/routes/products/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../../controllers/product.controller');
-const upload = require('../../middlewares/upload.middlewares');
+const { upload } = require('../../middlewares/upload.middlewares');
 const MAX_FILES = 10;
 
 // GET
@@ -18,7 +18,13 @@ router.post('/', (req, res, next) => {
 );
 
 // PUT
-router.put('/:id', productController.updateProduct);
+router.put('/:id', (req, res, next) => {
+    req.folder = process.env.PRODUCT_FOLDER; // Folder name in cloud
+    next();
+},
+    upload.array('images', MAX_FILES),
+    productController.updateProduct
+);
 
 // DELETE
 router.delete('/:id', productController.deleteProduct);
