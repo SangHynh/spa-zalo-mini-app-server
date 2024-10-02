83c30157b203f77fcc50ef0aef87fefdfe4a5865
MPhong03 <dangminhphong912@gmail.com>
2024-10-02 16:12:05 +0700

    doc: describe payment api

--- server/routes/payments/index.js
+++ server/routes/payments/index.js
@@ -6,18 +6,58 @@ const paymentController = require('../../controllers/payment.controller')
 router.get('/:id', paymentController.getOrder)
 
 // POST
-/* mac gồm 3 loại: 
-1. Dành cho phương thức Payment.createOrder 
-2. Dàng cho API getOrderStatus
-3. Dành cho API updateOrderStatus
-4. Dành cho API createRefund
-5. Dành cho API getRefundStatus
-*/
-router.post('/createMac', paymentController.createMac);
-router.post('/order/get/createMac', paymentController.createMacForGetOrderStatus);
-router.post('/order/update/createMac', paymentController.createMacForUpdateOrderStatus);
+// mac gồm 3 loại: 
+router.post('/createMac', paymentController.createMac); // 1. Dành cho phương thức Payment.createOrder 
+router.post('/order/get/createMac', paymentController.createMacForGetOrderStatus); // 2. Dàng cho API getOrderStatus
+router.post('/order/update/createMac', paymentController.createMacForUpdateOrderStatus); // 3. Dành cho API updateOrderStatus
 
+// notify API cho COD hoặc BANK method
 router.post('/zaloNotify', paymentController.zaloNotify);
+
+// POST
+/* có 2 loại body:
+{
+    "bookingId": , // null nếu không phải booking
+    "customerId": ,
+    "orderDate": ,
+    "totalAmount": , // tổng tiền
+    "discountApplied": ,
+    "discountAmount": ,
+    "finalAmount": , // tổng tiền đã giảm
+    "paymentMethod": ,
+    "paymentStatus": ,
+    "transactionId": , // orderId của zalo cấp (có thể thêm sau)
+    "remarks": ,
+    "referralId": ,
+    "products": , hoặc "items": ,
+    "voucherId": ,
+}
+
+Trong đó:
+
+"products": [
+    {
+        "productId":
+        "productName":
+        "price":
+        "quantity":
+    },
+]
+
+hoặc
+
+"items": [
+    {
+        "product": {
+            "id": ,
+            "name": ,
+            "price": 
+        },
+        "quantity": 
+    },
+] // Vì Zalo yêu cầu
+
+*/
 router.post('/createOrder', paymentController.createOrder);
 
 // PUT
