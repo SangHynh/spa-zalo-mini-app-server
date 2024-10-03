26c9b3fae1f383dffb19d3e30f9f4ff0a57c7fd6
MPhong03 <dangminhphong912@gmail.com>
2024-10-03 20:45:56 +0700

    feat: app config api for app slider

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -7,6 +7,8 @@ const voucher = require('./vouchers')
 const payment = require('./payments')
 const booking = require('./bookings')
 const recommendations = require('./recommendations')
+const configs = require('./app')
+
 const initRoutes = (app) => {
     app.use('/api/products', product)
     app.use('/api/users', user)
@@ -17,6 +19,7 @@ const initRoutes = (app) => {
     app.use('/api/payments', payment)
     app.use('/api/bookings', booking)
     app.use('/api/recommendations',recommendations)
+    app.use('/api/configs', configs)
 }
 
 
